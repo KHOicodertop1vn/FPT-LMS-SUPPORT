b64cb14d3db19938229e5fda6fa99b7f818479ff
KHOicodertop1vn <buidangkhoi28@gmail.com>
2026-09-25 12:25:38 +0700

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/FPT-LMS-SUPPORT/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
